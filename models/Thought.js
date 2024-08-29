8e11918f11665782d8c739856e10bde2e58b2a17
Adam <aorf@femmeosage.net>
2024-08-29 14:08:22 -0500

    updated for virtuals

--- models/Thought.js
+++ models/Thought.js
@@ -1,39 +1,51 @@
 const { Schema, model } = require("mongoose");
 
-const thoughtSchema = new Schema({
-  thoughtText: {
+const reactionSchema = new Schema({
+  reactionBody: {
     type: String,
     required: true,
-    minlength: 1,
     maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
   },
   createdAt: {
     type: Date,
     default: Date.now,
     get: (timestamp) => new Date(timestamp).toLocaleString(),
   },
-  username: {
-    type: String,
-    required: true,
-  },
-  reactions: [
-    {
-      reactionBody: {
-        type: String,
-        required: true,
-        maxlength: 280,
-      },
-      username: {
-        type: String,
-        required: true,
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now,
-        get: (timestamp) => new Date(timestamp).toLocaleString(),
-      },
+});
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
     },
-  ],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => new Date(timestamp).toLocaleString(),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
 });
 
 const Thought = model("Thought", thoughtSchema);
