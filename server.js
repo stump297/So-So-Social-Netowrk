f1e220e0733b15b0c7159d5e463f092c4f816bba
Adam <aorf@femmeosage.net>
2024-08-29 14:54:35 -0500

    added routes to server.js

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const thoughtRoutes = require("./routes/thoughtRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use("/api/users", userRoutes);
+app.use("/api/thoughts", thoughtRoutes);
 
 app.use(express.json());
 
