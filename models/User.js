c1c40dc0a9659d9f9a6d7f6e9cc8a4b6b090c238
Adam <aorf@femmeosage.net>
2024-08-29 13:40:08 -0500

    set up user model

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must match an email address!"],
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
