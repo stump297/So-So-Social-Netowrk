220fab80e725976731dae235dfebcda707e02996
Adam <aorf@femmeosage.net>
2024-08-30 13:24:17 -0500

    updated server.js

--- server.js
+++ server.js
@@ -4,16 +4,13 @@ const userRoutes = require("./routes/userRoutes");
 const thoughtRoutes = require("./routes/thoughtRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/thoughts", thoughtRoutes);
 
-mongoose.connect("mongodb://localhost:27017/socialNetworkDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect("mongodb://localhost:27017/socialNetworkDB", {});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
